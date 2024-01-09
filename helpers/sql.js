ab288887a1a56791755cf70c6d0e620ed5251a82
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-01-09 03:57:57 -0600

    Refactor Company model and add example for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -6,6 +6,13 @@ const { BadRequestError } = require("../expressError");
 // JavaScript-style property names to SQL column names. It returns an object
 // containing a string for the SQL SET clause and an array of the new values.
 
+// @example
+// sqlForPartialUpdate(
+//   { firstName: 'Aliya', age: 32 },
+//   { firstName: 'first_name' }
+// );
+// Returns: { setCols: '"first_name"=$1, "age"=$2', values: ['Aliya', 32] }
+
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
