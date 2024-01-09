302a253eebc8d7029936efd706308adf79327fc3
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-01-09 03:39:47 -0600

    Refactor database URI and improve documentation in config.js and sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,14 +1,18 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+// Generate a SQL query for a partial update.
+
+// The function takes an object of data to be updated and an object that maps
+// JavaScript-style property names to SQL column names. It returns an object
+// containing a string for the SQL SET clause and an array of the new values.
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
